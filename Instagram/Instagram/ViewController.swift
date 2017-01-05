5d079db0ab8355f5cf612e102e2780542ded1a08
shekodn <sdiazn19@gmail.com>
2017-01-05 17:45:29 -0600

    makes new xcode project

---
+++ Instagram/Instagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Sergio Díaz Navarro on 1/5/17.
+//  Copyright © 2017 Sergio Díaz Navarro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
