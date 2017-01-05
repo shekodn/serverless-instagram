3a96ffa4f36853a6a5e0a10cf0b11b975d92addc
shekodn <sdiazn19@gmail.com>
2017-01-05 17:51:50 -0600

    adds bridging successfully

---
+++ Instagram/Instagram/Instagram-Bridging-Header.h
@@ -0,0 +1,20 @@
+//
+//  Instagram-Bridging-Header.h
+//  Instagram
+//
+//  Created by Sergio Díaz Navarro on 1/5/17.
+//  Copyright © 2017 Sergio Díaz Navarro. All rights reserved.
+//
+
+#ifndef Instagram_Bridging_Header_h
+#define Instagram_Bridging_Header_h
+
+
+#import <AWSCore/AWSCore.h>
+#import <AWSCognito/AWSCognito.h>
+#import <AWSDynamoDB/AWSDynamoDB.h>
+#import <AWSS3/AWSS3.h>
+
+
+
+#endif /* Instagram_Bridging_Header_h */
