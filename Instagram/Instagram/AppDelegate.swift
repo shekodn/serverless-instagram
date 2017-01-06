7777b08a8f1fd54a06d4bfd439210a4f16a548ed
shekodn <sdiazn19@gmail.com>
2017-01-05 18:50:08 -0600

    adds google sign in part 2

--- Instagram/Instagram/AppDelegate.swift
+++ Instagram/Instagram/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleSignIn
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,8 +18,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        GIDSignIn.sharedInstance().clientID = "473236537599-bsms48hhkk4b4jg6svjdsrabvk6eje1i.apps.googleusercontent.com"
+
+        
         return true
     }
+    
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        print(options)
+        return GIDSignIn.sharedInstance().handle(url,
+                                                 sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String,
+                                                 annotation: options[UIApplicationOpenURLOptionsKey.annotation])
+    }
+
+
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- Instagram/Instagram/ViewController.swift
+++ Instagram/Instagram/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import GoogleSignIn
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
+    
+    
+    
+    
+    // The sign-in flow has finished and was successful if |error| is |nil|.
+    public func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!){
+        
+        
+        if (error == nil) {
+            let idToken = user.authentication.idToken
+            let name = user.profile.name
+            let email = user.profile.email
+            
+            print(idToken, name, email)
+        } else {
+            print("\(error.localizedDescription)")
+        }
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance().shouldFetchBasicProfile = true
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
 
