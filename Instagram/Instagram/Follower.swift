d0102bcb5d6cef00505d3200166d95575eab7e4a
shekodn <sdiazn19@gmail.com>
2017-01-07 15:53:20 -0600

    changes NSObject to String in User and Follower classes

--- Instagram/Instagram/Follower.swift
+++ Instagram/Instagram/Follower.swift
@@ -1,48 +1,48 @@
-////
-////  Follower.swift
-////
-////  Created by Sergio Diaz
-////  Copyright © 2016 Sergio Diaz. All rights reserved.
-////
 //
-//import Foundation
+//  Follower.swift
 //
-//class Follower : AWSDynamoDBObjectModel, AWSDynamoDBModeling {
-//    
-//    var id  : String = ""
-//    var follower  : String = ""
-//    var following   : String = ""
-//    
-//    override init!() {
-//        super.init()
-//    }
-//    
-//     init(dictionary dictionaryValue: [NSObject : AnyObject]!, error: ()) throws {
-//        super.init()
-//        
-//        if dictionaryValue["id"] != nil {
-//            id = dictionaryValue["id"] as! String
-//        }
-//        
-//        if dictionaryValue["follower"] != nil {
-//            follower = dictionaryValue["follower"] as! String
-//        }
-//        
-//        if dictionaryValue["following"] != nil {
-//            following = dictionaryValue["following"] as! String
-//        }
-//    }
-//    
-//    
-//    required init!(coder: NSCoder!) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//    
-//    class func dynamoDBTableName() -> String {
-//        return "Followers"
-//    }
-//    
-//    class func hashKeyAttribute() -> String {
-//        return "id"
-//    }
-//}
+//  Created by Sergio Diaz
+//  Copyright © 2016 Sergio Diaz. All rights reserved.
+//
+
+import Foundation
+
+class Follower : AWSDynamoDBObjectModel, AWSDynamoDBModeling {
+    
+    var id  : String = ""
+    var follower  : String = ""
+    var following   : String = ""
+    
+    override init!() {
+        super.init()
+    }
+    
+     init(dictionary dictionaryValue: [String : AnyObject]!, error: ()) throws {
+        super.init()
+        
+        if dictionaryValue["id"] != nil {
+            id = dictionaryValue["id"] as! String
+        }
+        
+        if dictionaryValue["follower"] != nil {
+            follower = dictionaryValue["follower"] as! String
+        }
+        
+        if dictionaryValue["following"] != nil {
+            following = dictionaryValue["following"] as! String
+        }
+    }
+    
+    
+    required init!(coder: NSCoder!) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    class func dynamoDBTableName() -> String {
+        return "Followers"
+    }
+    
+    class func hashKeyAttribute() -> String {
+        return "id"
+    }
+}
